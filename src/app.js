d4c777b8bc25c29d08b7de90f5e4b3fdd2325606
NISHIZAWA Shuntaro <me@s2n.tech>
2025-04-12 13:53:29 +0900

    feat: 初期テンプレートを追加

---
+++ src/app.js
@@ -0,0 +1,16 @@
+import { Client } from "discord.js";
+
+const client = new Client({
+  intents: ["Guilds", "GuildMembers", "GuildMessages", "MessageContent"],
+});
+
+client.on("ready", () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on("messageCreate", async (message) => {
+  if (message.author.bot) return;
+  console.log("Message received:", message.content);
+});
+
+client.login(process.env["DISCORD_BOT_TOKEN"]);
